01155ac7fd04f17432aab9910c3fc9d991e3527c
perillaroc <perillaroc@gmail.com>
2019-01-01 09:28:25 +0800

    :label: bump to v4.0.0 and happy new year.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='nwpc-sms-collector',
 
-    version='0.2.0',
+    version='1.0.0',
 
     description='An SMS collector at NWPC.',
     long_description=long_description,
